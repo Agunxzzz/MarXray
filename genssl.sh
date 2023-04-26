a7104ce73d82dd6ec0a3a5e4381ed95b48651bff
Agunxzzz <86614702+Agunxzzz@users.noreply.github.com>
2023-04-26 12:04:10 +0000

    Update genssl.sh

--- genssl.sh
+++ genssl.sh
@@ -1,6 +1,4 @@
 clear
-systemctl stop nginx
-systemctl stop xray
 domain=$(cat /etc/xray/domain)
 Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
 if [[ ! -z "$Cek" ]]; then
@@ -13,11 +11,13 @@ sleep 1
 fi
 echo -e "[ ${GREEN}INFO${NC} ] Starting renew gen-ssl... " 
 sleep 2
-/root/.acme.sh/acme.sh --upgrade
-/root/.acme.sh/acme.sh --upgrade --auto-upgrade
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+yum install curl socat -y
+curl https://get.acme.sh | sh
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+~/.acme.sh/acme.sh --register-account -m resaananta42@gmail.com
+~/.acme.sh/acme.sh --issue -d $domain --standalone
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /etc/xray/xray.key --fullchain-file /etc/xray/xray.crt
+
 echo -e "[ ${GREEN}INFO${NC} ] Renew gen-ssl done... " 
 sleep 2
 echo -e "[ ${GREEN}INFO${NC} ] Starting service $Cek " 
