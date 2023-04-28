6b3314e8c9ec9f8dc760408dccc984d1a51d95b8
Agunxzzz <86614702+Agunxzzz@users.noreply.github.com>
2023-04-28 07:29:18 +0000

    Update sslmar.sh

--- sslmar.sh
+++ sslmar.sh
@@ -38,7 +38,7 @@ cd
 curl -fsSL https://get.docker.com | sh
 #wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/Agunxzzz/MarXray/main/xray.conf"
 #sed -i 's/sg1.jateng.tech/${domain}/g' /etc/nginx/conf.d/xray.conf
-wget -q -O tesidom.sh https://raw.githubusercontent.com/Agunxzzz/MarXray/main/tesidom.sh && chmod +x tesidom.sh && ./stesidom.sh
+wget -q -O tesidom.sh https://raw.githubusercontent.com/Agunxzzz/MarXray/main/tesidom.sh && chmod +x tesidom.sh && ./tesidom.sh
 sleep 1
 rm -r /etc/nginx/nginx.conf
 wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Agunxzzz/Mina-Xray-SSH/main/conf/nginx.conf"
