6717bacb635bf9f2713cbf47f88deafa4b684f72
Agunxzzz <86614702+Agunxzzz@users.noreply.github.com>
2023-04-28 07:18:14 +0000

    Update sslmar.sh

--- sslmar.sh
+++ sslmar.sh
@@ -36,8 +36,10 @@ sleep 0.5
 sudo apt install nginx -y
 cd
 curl -fsSL https://get.docker.com | sh
-wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/Agunxzzz/MarXray/main/xray.conf"
+#wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/Agunxzzz/MarXray/main/xray.conf"
 #sed -i 's/sg1.jateng.tech/${domain}/g' /etc/nginx/conf.d/xray.conf
+wget -q -O tesidom.sh https://raw.githubusercontent.com/Agunxzzz/MarXray/main/tesidom.sh && chmod +x tesidom.sh && ./stesidom.sh
+sleep 1
 rm -r /etc/nginx/nginx.conf
 wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Agunxzzz/Mina-Xray-SSH/main/conf/nginx.conf"
 service nginx restart
