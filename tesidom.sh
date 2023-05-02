a4069d1173104186d8d0685bfca67373626cbbb8
Agunxzzz <86614702+Agunxzzz@users.noreply.github.com>
2023-05-02 16:47:08 +0700

    Update tesidom.sh

--- tesidom.sh
+++ tesidom.sh
@@ -61,7 +61,7 @@ proxy_set_header Host $http_host;
 location = /ss-ws
 {
 proxy_redirect off;
-proxy_pass http://127.0.0.1:2096;
+proxy_pass http://127.0.0.1:2016;
 proxy_http_version 1.1;
 proxy_set_header X-Real-IP $remote_addr;
 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
